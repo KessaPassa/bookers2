79fe4f9ecdd49a1d4dee464accf54e698cb5d42d
KessaPassa <k.someya.gyao@gmail.com>
2023-06-11 17:42:04 +0900

    add: わからなくなるのでコメントを追加する

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Relationship < ApplicationRecord
-  belongs_to :followed, class_name: 'User'
-  belongs_to :follower, class_name: 'User'
+  belongs_to :followed, class_name: 'User' # フォローするをされたユーザ
+  belongs_to :follower, class_name: 'User' # フォローするをしたユーザ
 
   class << self
     def create_or_destroy!(followed:, follower:)
